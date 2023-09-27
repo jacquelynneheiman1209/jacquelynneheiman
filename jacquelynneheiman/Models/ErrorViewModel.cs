a74e6a9c9426aeb69f0bbf7cde7e71a975e3a72e
jacquelynneheiman1209 <jacquelynneheiman1209@gmail.com>
2023-09-26 22:14:29 -0700

    Started the portfolio site and added the navigation bar

---
+++ jacquelynneheiman/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jacquelynneheiman.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ jacquelynneheiman/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿
+const body = document.querySelector("body");
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".toggle");
+const searchBox = document.querySelector(".search-box");
+const modeSwitch = document.querySelector(".toggle-switch");
+const modeText = document.querySelector(".mode-text");
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
