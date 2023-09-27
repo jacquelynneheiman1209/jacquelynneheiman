812194d60f03a49bec9bb7d5c0b9cb1d28bf24d3
jacquelynneheiman1209 <jacquelynneheiman1209@gmail.com>
2023-09-27 09:06:02 -0700

    finished adding the navigation menu, started on the home page layout

--- jacquelynneheiman/wwwroot/js/site.js
+++ jacquelynneheiman/wwwroot/js/site.js
@@ -2,10 +2,27 @@
 const body = document.querySelector("body");
 const sidebar = document.querySelector(".sidebar");
 const toggle = document.querySelector(".toggle");
-const searchBox = document.querySelector(".search-box");
+const searchButton = document.querySelector(".search-box");
+const searchBox = document.querySelector(".search-bar");
 const modeSwitch = document.querySelector(".toggle-switch");
 const modeText = document.querySelector(".mode-text");
 
+searchButton.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+    searchBox.focus();
+});
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light Mode";
+    }
+    else {
+        modeText.innerText = "Dark Mode";
+    }
 });
